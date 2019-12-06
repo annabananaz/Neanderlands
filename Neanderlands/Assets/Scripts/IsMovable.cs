0c9670452b5233091a3197a27157a82b5119c4ab
Isaac Torres <torresisaac26@gmail.com>
2019-12-06 15:33:17 -0800

    change to move when hands selected

---
+++ Neanderlands/Assets/Scripts/IsMovable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsMovable : MonoBehaviour
+{
+	[SerializeField]
+	InventoryControl inventoryControl;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (inventoryControl.currentTool == 1)
+		{
+			GetComponent<Rigidbody>().mass = 1;
+		}
+		else {
+			GetComponent<Rigidbody>().mass = 1000;
+		}       
+    }
+}
