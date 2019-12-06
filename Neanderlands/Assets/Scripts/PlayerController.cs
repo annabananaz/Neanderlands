cfa64d3cc70d96e9a10fb55238e3a9fdbf562af8
Eric Gonzalez Olivares <gonz9019@csumb.ad.csumb.edu>
2019-12-05 21:04:02 -0800

    merging isaac's branch to master

--- Neanderlands/Assets/Scripts/PlayerController.cs
+++ Neanderlands/Assets/Scripts/PlayerController.cs
@@ -139,7 +139,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    IEnumerator OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Lava")
         {
