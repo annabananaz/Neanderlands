1a1d73b21b073bc440dbd31986d53c349fc35f78
Isaac Torres <torresisaac26@gmail.com>
2019-12-06 16:56:07 -0800

    fixed bug

--- Neanderlands/Assets/Scripts/InventoryControl.cs
+++ Neanderlands/Assets/Scripts/InventoryControl.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class InventoryControl : MonoBehaviour
 {
@@ -14,24 +13,6 @@ public class InventoryControl : MonoBehaviour
     public bool PickAxe = false;
     public bool Torch = false;
 
-    //ui elements for each tool
-    public Graphic hands;
-    public Graphic pick;
-    public Graphic torch;
-    public Graphic handsBG;
-    public Graphic pickBG;
-    public Graphic torchBG;
-
-    void Awake()
-    {
-        //only hands are visible
-        handsBG.color = Color.white;
-        pickBG.color = Color.black;
-        pick.CrossFadeAlpha(0, 0.0f, false);
-        torchBG.color = Color.black;
-        torch.CrossFadeAlpha(0, 0.0f, false);
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -40,25 +21,16 @@ public class InventoryControl : MonoBehaviour
         if (Input.GetKeyDown("1"))
         {
             currentTool = 1;
-            handsBG.color = Color.white;
-            pickBG.color = Color.black;
-            torchBG.color = Color.black;
             print("Current Tool is Hands");
         }
         if (Input.GetKeyDown("2") && PickAxe == true)
         {
             currentTool = 2;
-            handsBG.color = Color.black;
-            pickBG.color = Color.white;
-            torchBG.color = Color.black;
             print("Current Tool is PickAxe");
         }
         if (Input.GetKeyDown("3") && Torch == true)
         {
             currentTool = 3;
-            handsBG.color = Color.black;
-            pickBG.color = Color.black;
-            torchBG.color = Color.white;
             print("Current Tool is Torch");
         }
 
@@ -86,24 +58,18 @@ public class InventoryControl : MonoBehaviour
             float distPickaxe = Vector3.Distance(gameObject.transform.position, pickaxeFind.transform.position);
             if (distPickaxe <= 1.5f)
             {
+                print("pickaxe pickup");
                 PickAxe = true;
                 currentTool = 2;
-                pick.CrossFadeAlpha(1, 0.0f, false);
-                handsBG.color = Color.black;
-                pickBG.color = Color.white;
-                torchBG.color = Color.black;
                 pickaxeFind.SetActive(false);
             }
 
             float distTorch = Vector3.Distance(gameObject.transform.position, torchFind.transform.position);
             if (distTorch <= 1.5f)
             {
+                print("pickaxe torch");
                 Torch = true;
                 currentTool = 3;
-                torch.CrossFadeAlpha(1, 0.0f, false);
-                handsBG.color = Color.black;
-                pickBG.color = Color.black;
-                torchBG.color = Color.white;
                 torchFind.SetActive(false);
             }
 
